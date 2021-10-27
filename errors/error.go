f95d9246f281609d1d05d065d247ac063df785f9
KimJeongChul <kjc5443@gmail.com>
2021-10-27 13:43:43 +0900

    [UPDATE] Add error code

--- errors/error.go
+++ errors/error.go
@@ -22,7 +22,6 @@ func (e *CError) Error() string {
 
 type ErrCode int
 
-
 const (
 	SUCCESS ErrCode = iota
 
@@ -41,6 +40,39 @@ const (
 	// HTTP Invalid method
 	HTTP_INVALID_METHOD_ERR
 
-	// AUTH error
+	// HTTP_AUTH_ERR Authorization error
 	HTTP_AUTH_ERR
+
+	// HTTP_PREPROCESSING_ERR Cannot processing
+	HTTP_PREPROCESSING_ERR
+
+	// HTTP_FLUSHER_ERR Create Flusher http.Flusher error
+	HTTP_FLUSHER_ERR
+
+	// HTTP_MULTIPART_PARSE_ERR HTTP Multipart Parse mime.ParseMediaType() error
+	HTTP_MULTIPART_PARSE_ERR
+
+	// HTTP_MULTIPART_READ_ERR HTTP Multipart multipartReader.NextPart() error
+	HTTP_MULTIPART_READ_ERR
+
+	// HTTP BODY READ NIL
+	HTTP_BODY_READ_ERR
+
+	// IOUTIL_READ_ERR ioutil.ReadAll() ioutil.ReadFile() error
+	IOUTIL_READ_ERR
+
+	// STT audio format
+	STT_FORMAT_ERR
+
+	// STT audio channel format
+	STT_CHANNEL_ERR
+
+	// STT audio samplerate
+	STT_SAMPLERATE_ERR
+
+	// JSON_MARSHAL_ERR JSON encoding json.Marshal() error
+	JSON_MARSHAL_ERR
+
+	// JSON_UNMARSHAL_ERR JSON decoding json.Unmarshal() error
+	JSON_UNMARSHAL_ERR
 )
