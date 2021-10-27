b8b3b7fd06f570a310f1b3b5c0daf897980b8505
KimJeongChul <kjc5443@gmail.com>
2021-10-27 15:41:51 +0900

    [Update] checkSpeechReqParams

--- api/preprocessing.go
+++ api/preprocessing.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 	"runtime"
 	"strings"
-	"voice-api-server/utils"
+	"voice-api-server/common"
 	"voice-api-server/errors"
 	"voice-api-server/logger"
-	"voice-api-server/common"
+	"voice-api-server/utils"
 )
 
-func (as *ApiServer) APIPreProcessing(w *http.ResponseWriter, req *http.Request)  (sessionObj *common.SessionObj, cErr *errors.CError, errCode int) {
+func (as *ApiServer) APIPreProcessing(w *http.ResponseWriter, req *http.Request) (sessionObj *common.SessionObj, cErr *errors.CError, errCode int) {
 	sessionObj = &common.SessionObj{}
 	var err error
 
@@ -48,4 +48,31 @@ func (as *ApiServer) APIPreProcessing(w *http.ResponseWriter, req *http.Request)
 	}
 
 	return
+}
+
+// checkSpeechReqParams Check valid parameters
+func checkSpeechReqParams(target MsgSpeechMetaData) (cErr *errors.CError) {
+	//encoding parameter check
+	if !(target.Encoding == "raw" || target.Encoding == "mp3" || target.Encoding == "vor" || target.Encoding == "wav" || target.Encoding == "aac" || target.Encoding == "fla") {
+		cErr = errors.NewCError(errors.STT_FORMAT_ERR, "not supported audio format")
+		return
+	}
+	// Check encoding is raw
+	if target.Encoding == "raw" {
+		//channel parameter check
+		if !(target.EncodingOpt.Channel == 1 || target.EncodingOpt.Channel == 2) {
+			cErr = errors.NewCError(errors.STT_CHANNEL_ERR, "not supported audio channel")
+			return
+		}
+		//sampleRate Check
+		if !(target.EncodingOpt.SampleRate == 16000 || target.EncodingOpt.SampleRate == 44100 || target.EncodingOpt.SampleRate == 48000) {
+			cErr = errors.NewCError(errors.STT_SAMPLERATE_ERR, "not supported sample rate")
+			return
+		}
+		if !(target.EncodingOpt.SampleFmt == "S16LE" || target.EncodingOpt.SampleFmt == "F32LE") {
+			cErr = errors.NewCError(errors.STT_FORMAT_ERR, "not supported audio format")
+			return
+		}
+	}
+	return nil
 }
